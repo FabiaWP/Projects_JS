4e947eb30158bbdce8ee8cbae3fbf4fbbe9b2754
Fabia Omezzoli <fabia.omezzoli@gmail.com>
2016-08-27 14:42:43 +0200

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Name: Fabia JScript
+  Plugin URI:
+
+ */
+define('FO_JSCRIPT_PATH', dirname(__FILE__));
+define('FO_JSCRIPT_FOLDER', basename(FO_JSCRIPT_PATH));
+define('FO_JSCRIPT_URL', plugins_url() . '/' . FO_JSCRIPT_FOLDER);
+
+
+add_action('init','enqueue_scripts_2');
+
+
+function enqueue_scripts_2() {
+        wp_register_script( 'jscript', FO_JSCRIPT_URL . '/res/js/main.js' );
+
+
+        wp_enqueue_script('jscript');
+    }
+
+$post = isset($_GET['post']) ? $_GET['post'] : false;  // trovo l'id
+
+
+
+echo ' casososososos';
+$a=2;
+var_export($a);
+
+if($post) {
+
+
+
+    add_filter('wpt_field_options', 'fo_members_options', 10, 2);
+
+}
+
+
---
+++ res/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by fabia on 27/08/16
+ */
+jQuery(document).ready(function () {
+console.log("Helloadada");
+});
